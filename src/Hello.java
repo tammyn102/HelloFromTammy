576bf0363d72fc87b6c90c9a737921f5824e5e86
tammyn102 <65318171+tammyn102@users.noreply.github.com>
2020-05-15 14:34:03 -0600

    Code for HelloFromTammy

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
         public static void main(String[] args)
         {
-            System.out.println("Hello, my name is Tammy");
+            System.out.println("Hello Alex, Derek, Joshua , my name is Tammy");
         }
 
 }
