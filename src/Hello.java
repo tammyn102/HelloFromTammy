55a8b644c8444bba96a3ad559bd84d0fea85e3ed
Cellphone104 <53287422+dmuller104@users.noreply.github.com>
2020-05-16 10:40:47 -0600

    Added a hello to Tammy I had to change the JDK to 14 for mine, should be easy to move back under file>>project structure>>project, change project sdk.

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
         public static void main(String[] args)
         {
             System.out.println("Hello Alex, Derek, Joshua , my name is Tammy");
+            System.out.println("Hello Tammy, my name is Derek!");
         }
 
 }
